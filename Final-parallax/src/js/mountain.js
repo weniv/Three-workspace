f239a968cd36f4fd058dbde9bfe6ae2de8466bd1
min-bok <leehy0782@gmail.com>
2023-04-20 13:55:07 +0900

    다중 scene 적둉전 저장

---
+++ Final-parallax/src/js/mountain.js
@@ -0,0 +1,28 @@
+import * as THREE from "../../node_modules/three/build/three.module.js";
+
+const printMoutain = () => {
+  // 텍스쳐 추가
+  const loader = new THREE.TextureLoader();
+
+  const meterials = [
+    // 옆면
+    new THREE.MeshBasicMaterial({
+      map: loader.load("../src/static/img/mountain.jpg"),
+    }),
+    //
+    new THREE.MeshBasicMaterial({
+      map: loader.load("../src/static/img/mountain_top.jpg"),
+    }),
+    // 아랫면
+    new THREE.MeshBasicMaterial({
+      color: 0x609966,
+    }),
+  ];
+
+  const geometry = new THREE.CylinderGeometry(0.6, 2, 2, 8);
+  const moutain = new THREE.Mesh(geometry, meterials);
+
+  return moutain;
+};
+
+export default printMoutain();
