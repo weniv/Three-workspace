6c50777640771cf71f2e926013fda8eec46e813e
min-bok <leehy0782@gmail.com>
2023-04-27 11:27:20 +0900

    island에 house 추가

--- Final-parallax/src/js/island.js
+++ Final-parallax/src/js/island.js
@@ -2,11 +2,13 @@ import * as THREE from "three";
 import ground from "./ground.js";
 import { cloudGrp } from "./cloud.js";
 import { dolhareubangGrp } from "./stone.js";
+import house from "./house.js";
 
 const printIsland = () => {
   const island = new THREE.Group();
 
   island.add(ground);
+  island.add(house);
   island.add(cloudGrp);
   island.add(dolhareubangGrp);
 
