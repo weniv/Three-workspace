f563b0e02f0b99ef2a0b6adaefb47e073700cc8a
min-bok <leehy0782@gmail.com>
2023-05-09 14:06:47 +0900

    Edit: add house texture

--- src/js/mountain.js
+++ src/js/mountain.js
@@ -10,11 +10,15 @@ const printMoutain = () => {
   grassBase.wrapS = THREE.RepeatWrapping;
   grassBase.repeat.set(5, 2);
 
-  const meterials = [
-    new THREE.MeshStandardMaterial({
-      map: grassBase,
-    }),
-  ];
+  // const meterials = [
+  //   new THREE.MeshStandardMaterial({
+  //     map: grassBase,
+  //   }),
+  // ];
+
+  const meterials = new THREE.MeshStandardMaterial({
+    map: grassBase,
+  });
 
   const geometry = new THREE.CylinderGeometry(0.6, 2, 2, 8);
   const moutain = new THREE.Mesh(geometry, meterials);
